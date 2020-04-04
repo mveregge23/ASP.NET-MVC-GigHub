47ca1f4aab1b2c35c38d798d5fa8408a000170b9
Max Veregge <maxwell.veregge@gmail.com>
2020-04-04 13:41:50 -0400

    implementation notification api

---
+++ GigHub/Controllers/api/NotificationsController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.api
+{
+
+    [Authorize]
+    public class NotificationsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NotificationsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<NotificationDto> GetNewNotifications()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId && un.IsRead == false)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+            Mapper.CreateMap<ApplicationUser, UserDto>();
+            Mapper.CreateMap<Gig, GigDto>();
+            Mapper.CreateMap<Notification, NotificationDto>();
+
+            return notifications.Select(Mapper.Map<Notification, NotificationDto>);
+        }
+    }
+}
---
+++ GigHub/Dtos/GenreDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GigHub.Dtos
+{
+    public class GenreDto
+    {
+        public byte Id { get; set; }
+        public String name { get; set; }
+    }
+}
